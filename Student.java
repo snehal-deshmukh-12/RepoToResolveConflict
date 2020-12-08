3158bbcd87549df452b6cc27bc7725b60d33b592
cah-sweety-gholap <sweety.gholap@cardinalhealth.com>
2020-12-08 18:54:17 +0530

    student

--- Student.java
+++ Student.java
@@ -1,4 +1,36 @@
 
-public class Student {
-
+public class Student 
+{
+	String name;
+	int age;
+	String city;
+	public Student(String name, int age, String city) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.city = city;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	@Override
+	public String toString() {
+		return "Student [name=" + name + ", age=" + age + ", city=" + city + "]";
+	}
+	
 }
